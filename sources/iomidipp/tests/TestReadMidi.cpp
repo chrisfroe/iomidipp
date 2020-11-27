f8dfa005068622e82438806c1b13ff7b85ebd757
Christoph Fröhner <christoph.froehner@posteo.net>
2020-11-27 15:34:25 +0100

    github actions for build and test, cmake target alias

--- sources/iomidipp/tests/TestReadMidi.cpp
+++ sources/iomidipp/tests/TestReadMidi.cpp
@@ -2,27 +2,35 @@
 #include <iomidipp/MidiFile.h>
 #include <iostream>
 
-TEST_CASE("Foo", "") {
-    imp::MidiData midiData = imp::File::read("testdata/scratch.mid");
-    midiData.doTimeAnalysis();
-    midiData.linkNotePairs();
+TEST_CASE("Read midi file and print output") {
+    WHEN("Example file is read and we do common operations") {
+        imp::MidiData midiData = imp::File::read("testdata/scratch.mid");
+        midiData.doTimeAnalysis();
+        midiData.linkNotePairs();
 
-    int numberOfTracks = midiData.getNumberOfTracks();
-    std::cout << "TPQ: " << midiData.getTicksPerQuarterNote() << "\n";
-    if (numberOfTracks > 1) std::cout << "TRACKS: " << numberOfTracks << "\n";
-    for (int track = 0; track < numberOfTracks; track++) {
-        if (numberOfTracks > 1) std::cout << "\nTrack " << track << "\n";
-        std::cout << "Tick\tSeconds\tDur\tMessage" << "\n";
-        for (int event = 0; event < midiData[track].size(); event++) {
-            std::cout << std::dec << midiData[track][event].tick;
-            std::cout << '\t' << std::dec << midiData[track][event].seconds;
-            std::cout << '\t';
-            if (midiData[track][event].isNoteOn())
-                std::cout << midiData[track][event].getDurationInSeconds();
-            std::cout << '\t' << std::hex;
-            for (int i = 0; i < midiData[track][event].getSize(); i++)
-                std::cout << (int) midiData[track][event][i] << ' ';
-            std::cout << "\n";
+        std::stringstream ss;
+        
+        int numberOfTracks = midiData.getNumberOfTracks();
+        ss << "TPQ: " << midiData.getTicksPerQuarterNote() << "\n";
+        if (numberOfTracks > 1) ss << "TRACKS: " << numberOfTracks << "\n";
+        for (int track = 0; track < numberOfTracks; track++) {
+            if (numberOfTracks > 1) ss << "\nTrack " << track << "\n";
+            ss << "Tick\tSeconds\tDur\tMessage" << "\n";
+            for (int event = 0; event < midiData[track].size(); event++) {
+                ss << std::dec << midiData[track][event].tick;
+                ss << '\t' << std::dec << midiData[track][event].seconds;
+                ss << '\t';
+                if (midiData[track][event].isNoteOn())
+                    ss << midiData[track][event].getDurationInSeconds();
+                ss << '\t' << std::hex;
+                for (int i = 0; i < midiData[track][event].getSize(); i++)
+                    ss << (int) midiData[track][event][i] << ' ';
+                ss << "\n";
+            }
+        }
+        THEN("Nothing bad happens along the way") {
+            REQUIRE(true);
+            // std::cout << ss.str() << std::endl;
         }
     }
 }
